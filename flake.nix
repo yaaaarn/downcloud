459ee59387a7dbe5e0382be112cfe8f9e006a847
yaaaarn <30006414+yaaaarn@users.noreply.github.com>
2026-07-13 08:45:09 +0200

    removed flake utils

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
-  description = "downcloud - a simple (and fast) soundcloud downloader";
+  description = "downcloud";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     bun2nix.url = "github:nix-community/bun2nix";
     bun2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -12,34 +11,49 @@
     {
       self,
       nixpkgs,
-      flake-utils,
       bun2nix,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        bun2nix' = bun2nix.packages.${system}.default;
-      in
-      {
-        packages.default = pkgs.callPackage ./default.nix { bun2nix = bun2nix'; };
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; lib.optionals (!stdenv.isDarwin) [ libsecret ];
-
-          packages = with pkgs; [
-            bun
-            bun2nix'
-          ];
-
-          shellHook = ''
-            export LD_LIBRARY_PATH="${
-              pkgs.lib.makeLibraryPath (
-                (pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [ pkgs.libsecret ]) ++ [ pkgs.glib ]
-              )
-            }:$LD_LIBRARY_PATH"
-          '';
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+ 
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      packages = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.callPackage ./default.nix {
+          bun2nix = bun2nix.packages.${system}.default;
         };
-      }
-    );
+      });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          bun2nix' = bun2nix.packages.${system}.default;
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; lib.optionals (!stdenv.isDarwin) [ libsecret ];
+
+            packages = with pkgs; [
+              bun
+              bun2nix'
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${
+                pkgs.lib.makeLibraryPath (
+                  (pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [ pkgs.libsecret ]) ++ [ pkgs.glib ]
+                )
+              }:$LD_LIBRARY_PATH"
+            '';
+          };
+        }
+      );
+    };
 }
